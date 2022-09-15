79b1241564fd35e8d2597728e419d6bddd75294a
Houdja <houdja-ahamadi976@hotmail.com>
2022-09-15 14:49:25 +0400

    pseudo code du script

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.contact__form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    //lorsque je soumet le formulaire j'évite le comportement par defaut
+    //je récupere les data du formulaire
+    //je soumet le formulaire
+    //je récupére la réponse 
+    //si statut ok afficher un message de success à l'utilisateur
+    //sinn capturer l'erreur et informer l'utilisateur de l'erreur
+
+    
+})
