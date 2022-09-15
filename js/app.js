0ab4a1a105022ca8b801ca03b3a79fab67e7cc83
Houdja <houdja-ahamadi976@hotmail.com>
2022-09-15 15:44:08 +0400

    requete ajax soumission formulaire

--- js/app.js
+++ js/app.js
@@ -9,5 +9,29 @@ form.addEventListener('submit', function(e){
     //si statut ok afficher un message de success à l'utilisateur
     //sinn capturer l'erreur et informer l'utilisateur de l'erreur
 
-    
+    const data = new FormData(this);
+
+    fetch('php/adduser.php', {
+        method: 'POST',
+        body: data
+    })
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }else{
+            console.log('Une erreur est survenue');
+        }
+    })
+    .then(data => {
+        if(data.successMsg != ''){
+            alert(data.successMsg);
+            form.querySelectorAll('input').forEach(input => {
+                input.value = '';
+            })
+            form.querySelector('textarea').value = '';
+        }else{
+            alert(data.errorMsg);
+        }
+    })
+
 })
